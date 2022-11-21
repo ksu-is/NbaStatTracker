a9853f1cc18f61852103e2a04cde795f9e4ce0be
Tyler Favors <111463901+tfavor14@users.noreply.github.com>
2022-11-20 22:52:27 -0500

    Line 2
    Added a base website so people know where all the information is coming from.

--- Tracker.py
+++ Tracker.py
@@ -1,1 +1,6 @@
 
+
+BASE_URL = 'https://www.espn.com/nba/'
+PLAYER_LIST_URL = 'http://www.espn.com/nba/players'
+PLAYERS_ID_URL = 'https://www.espn.com/nba/player/_/id/[ID]/[player]'
+PLAYERS_GAMELOG_URL = 'https://www.espn.com/nba/player/gamelog/_/id/[ID]/[player]'
