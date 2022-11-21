0194d312537d23c8511bb921f553d0c038ea422c
Tyler Favors <111463901+tfavor14@users.noreply.github.com>
2022-11-21 08:51:54 -0500

    Line 3
    I added a link to be able to show a list of all the teams that shows all the players on that team whenever you click on aa certain team.

--- Tracker.py
+++ Tracker.py
@@ -1,5 +1,4 @@
 
 BASE_URL = 'https://www.espn.com/nba/'
 PLAYER_LIST_URL = 'http://www.espn.com/nba/players'
-PLAYERS_ID_URL = 'https://www.espn.com/nba/player/_/id/[ID]/[player]'
-PLAYERS_GAMELOG_URL = 'https://www.espn.com/nba/player/gamelog/_/id/[ID]/[player]'
+
