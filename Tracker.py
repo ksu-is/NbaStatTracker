75f89715d14e4ddc101edd0cf40ae4f1754c9dc3
Tyler Favors <111463901+tfavor14@users.noreply.github.com>
2022-12-01 17:20:26 -0500

    Update Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -1,5 +1,13 @@
+import
 
 BASE_URL = 'https://www.espn.com/nba/'
 PLAYER_LIST_URL = 'http://www.espn.com/nba/players'
 PLAYERS_ID_URL = 'https://www.espn.com/nba/player/_/id/[ID]/[player]'
 PLAYERS_GAMELOG_URL = 'https://www.espn.com/nba/player/gamelog/_/id/[ID]/[player]'
+
+class
+
+class NBAPointGuardStats(NBAStats):
+    def keys():
+        return [ 'id', 'player_id', 'GP', 'MIN', 'PTS', 'FGM', 'FGA', 'FG%', '3PM', '3PA', '3P%','FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'EFF'] 
+      
